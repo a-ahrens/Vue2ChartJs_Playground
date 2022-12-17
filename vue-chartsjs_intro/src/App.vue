92abd3beb071f1209e7ae4c3a965f1ceab87a339
Anthony <anthony.l.ahrens@gmail.com>
2022-12-16 19:10:28 -0600

    First Bar Chart complete.

---
+++ vue-chartsjs_intro/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to the Vue Charts.JS Playground"/>
+    <bar-chart/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import BarChart from './components/BarChart.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld, BarChart
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-chartsjs_intro/src/components/BarChart.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+    <h3>Bar Chart with Local Data</h3>
+  <Bar :data="chartData" :options="chartOptions" />
+
+</div>
+</template>
+
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, CategoryScale, LinearScale)
+
+export default {
+    name: 'BarChart',
+    components: { Bar },
+  data() {
+    return {
+        chartData: {
+            labels: [ 'January', 'February', 'March'],
+            datasets: [
+                {
+                    label: 'Data One',
+                    backgroundColor: '#f87979',
+                    data: [40, 20, 12]
+                }
+            ]
+        },
+    };
+  }
+}
+</script>
+
+<style>
+
+</style>
